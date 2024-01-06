6b7dc607a07534a17cf7dd84d2a7e0245172c7f9
mr.yaniv.tal@gmail.com <mr.yaniv.tal@gmail.com>
2024-01-06 16:26:38 +0200

    added checkpointing callback

--- src/lightning_runner/lightning_train_runner.py
+++ src/lightning_runner/lightning_train_runner.py
@@ -1,5 +1,7 @@
 import numpy as np
-from lightning.pytorch.loggers import CSVLogger, TensorBoardLogger
+import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import TensorBoardLogger
 from torch.nn import BCEWithLogitsLoss
 from torch.optim import Adam
 from torch.utils.data import DataLoader, random_split
@@ -11,7 +13,6 @@ from src.lightning_runner.csv_logs_callback import CsvLogsCallback
 from src.lightning_runner.lightning_model_wrapper import LightningWrapper
 from src.models.model_getter import get_resnet18
 from src.util_functions import create_output_dir
-import lightning as L
 
 
 if __name__ == '__main__':
@@ -36,6 +37,7 @@ if __name__ == '__main__':
     train_split_th = int(np.floor(train_split_ratio * len(train_val_dataset)))
     train_dataset, valid_dataset = random_split(train_val_dataset, [train_split_th, len(train_val_dataset) - train_split_th])
 
+
     # --- Dataloaders
     train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
     valid_dataloader = DataLoader(valid_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
@@ -45,16 +47,19 @@ if __name__ == '__main__':
     optimizer = Adam(model.parameters())
     criterion = BCEWithLogitsLoss()
 
-    # --- Lightning wrapper module
+    # --- Lightning wrapper module and callbacks
     l_module = LightningWrapper(model, optimizer, criterion)
 
     logger = TensorBoardLogger(outputs_path, name='tb_logs')
 
     train_csv_log_path = str(outputs_path / Path('train_log.csv'))
     test_csv_log_path = str(outputs_path / Path('test_log.csv'))
+    checkpoint_path = str(outputs_path / Path('checkpoints'))
 
-    callbacks = [CsvLogsCallback(train_csv_log_path, train=True, validation=True, test=False),
-                 CsvLogsCallback(train_csv_log_path, train=False, validation=False, test=True)]
+    callbacks = [
+        CsvLogsCallback(train_csv_log_path, train=True, validation=True, test=False),   # train and validation logs
+        CsvLogsCallback(train_csv_log_path, train=False, validation=False, test=True),  # test logs
+        ModelCheckpoint(save_top_k=5, dirpath=checkpoint_path, monitor='validation_loss', filename='checkpoint_{epoch:02d}_{validation_loss:.2f}')]   # Model checkpoints
 
     trainer = L.Trainer(max_epochs=NUM_EPOCHS, logger=logger, callbacks=callbacks, check_val_every_n_epoch=1, num_sanity_val_steps=0)
     trainer.fit(model=l_module, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader)
