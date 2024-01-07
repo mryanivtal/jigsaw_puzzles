f60e46cec223d8d4192149d9835b15f66696dfce
mr.yaniv.tal@gmail.com <mr.yaniv.tal@gmail.com>
2024-01-07 12:38:12 +0200

    Improved csv reporter

--- src/lightning_runner/per_sample_csv_log_callback.py
+++ src/lightning_runner/per_sample_csv_log_callback.py
@@ -11,18 +11,19 @@ from torchmetrics.functional import confusion_matrix
 
 
 class PerSampleCsvLogCallback(L.Callback):
-    def __init__(self, save_file_path: str, train: bool, validation: bool, test: bool):
+    def __init__(self, save_file_path: str, train: bool, validation: bool, test: bool, file_per_epoch: bool=True):
         super(PerSampleCsvLogCallback, self).__init__()
 
         self.log_train = train
         self.log_test = test
         self.log_validation = validation
+        self.file_per_epoch = file_per_epoch
 
         self.save_file_path = save_file_path
         Path(save_file_path).parent.mkdir(exist_ok=True, parents=True)
 
         self.epoch_cumulative_metrics: dict = None
-        self.log_dataframe: pd.DataFrame = None
+        self.log_dataframes: dict = None
 
         print(f'PerSampleCsvLogCallback:')
         print(f'Running on: Train: {train}, Validation: {validation}, Test: {test}')
@@ -70,13 +71,16 @@ class PerSampleCsvLogCallback(L.Callback):
     def _reset_epoch_metrics(self, trainer_stage: str):
         self.epoch_cumulative_metrics[trainer_stage] = {'id': [], 'file_path': [], 'original_size': [],
                                                         'label': [], 'logit': torch.Tensor()}
+        if self.file_per_epoch:
+            self.log_dataframes[trainer_stage] = pd.DataFrame()
+
 
     def _update_epoch_metrics(self, pl_module: LightningModule, batch, trainer_stage: str):
         self.epoch_cumulative_metrics[trainer_stage]['id'] += batch[1]['id']
         self.epoch_cumulative_metrics[trainer_stage]['file_path'] += batch[1]['file_path']
         self.epoch_cumulative_metrics[trainer_stage]['original_size'] += batch[1]['original_size']
         self.epoch_cumulative_metrics[trainer_stage]['label'] += pl_module.current_step_data['labels'].squeeze().int().tolist()
-        self.epoch_cumulative_metrics[trainer_stage]['logit'] = torch.concat([self.epoch_cumulative_metrics[trainer_stage]['logit'], pl_module.current_step_data['logits'].squeeze()])
+        self.epoch_cumulative_metrics[trainer_stage]['logit'] = torch.concat([self.epoch_cumulative_metrics[trainer_stage]['logit'], pl_module.current_step_data['logits'].squeeze().cpu()])
 
     def _report_metrics(self, trainer_stage: str, epoch: int):
         epoch_log = pd.DataFrame()
@@ -95,13 +99,23 @@ class PerSampleCsvLogCallback(L.Callback):
         epoch_log['sigmoid'] = sigmoid
         epoch_log['pred'] = pred
 
-        self.log_dataframe = pd.concat([self.log_dataframe, epoch_log], axis=0)
-        self._save_log_to_csv()
+        self.log_dataframes[trainer_stage] = pd.concat([self.log_dataframes[trainer_stage], epoch_log], axis=0)
+        self._save_log_to_csv(trainer_stage, epoch)
 
-    def _save_log_to_csv(self):
-        self.log_dataframe.to_csv(self.save_file_path, index=False, sep='\t')
+    def _save_log_to_csv(self, trainer_stage: str, epoch: int):
+        if self.file_per_epoch:
+            file_path = str(self.save_file_path) + f'_{trainer_stage}_{epoch}'
+            self.log_dataframes[trainer_stage].to_csv(file_path, index=False, sep='\t')
+
+        else:
+            file_path = str(self.save_file_path) + f'_{trainer_stage}'
+            self.log_dataframes[trainer_stage].to_csv(file_path, index=False, sep='\t')
 
     def _reset_train_data(self):
-        self.log_dataframe = pd.DataFrame()
+        self.log_dataframes = {
+            'train': pd.DataFrame(),
+            'validation': pd.DataFrame(),
+            'test': pd.DataFrame()
+        }
         self.epoch_cumulative_metrics = {}
 
