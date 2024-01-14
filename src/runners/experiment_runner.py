ee223c9a5843794a3632bc4ab5378e5a0fe2bfab
mr.yaniv.tal@gmail.com <mr.yaniv.tal@gmail.com>
2024-01-14 11:43:15 +0200

    added configs

--- src/runners/experiment_runner.py
+++ src/runners/experiment_runner.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # default_test_config = Path(__file__).parent / Path('run_configs/image_classification/jigsaw_classification_template.json')
-    default_test_config = Path(__file__).parent / Path('run_configs/patch_adgacence/patch_adgacence_template.json')
+    default_test_config = Path(__file__).parent / Path('run_configs/patch_adjacence/patch_adgacence_template.json')
 
     run_config_path = args.run_config if args.run_config is not None else default_test_config
     project_path = args.project_path if args.project_path is not None else env_constants.PROJECT_PATH
