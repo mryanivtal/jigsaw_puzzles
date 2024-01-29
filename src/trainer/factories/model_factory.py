30403e503d5252655a79f9681e2b314c8d454151
mr.yaniv.tal@gmail.com <mr.yaniv.tal@gmail.com>
2024-01-29 19:52:04 +0200

    Added ViT model with pos emb deactivated

--- src/trainer/factories/model_factory.py
+++ src/trainer/factories/model_factory.py
@@ -2,6 +2,9 @@ import torch
 import torchvision
 from torchvision.models import ViT_B_16_Weights, VisionTransformer
 
+from src.models.vision_transformer_revised import vit_b_16_noemb as vit_b_16_without_pos_emb
+from src.models.vision_transformer_revised import VisionTransformer as VisionTransformerWithoutPosEmb
+
 
 def get_model(params: dict):
     if params['name'] == 'resnet18':
@@ -10,6 +13,11 @@ def get_model(params: dict):
         model = get_vit_b16_224(params)
     elif params['name'] == 'custom_vision_transformer':
         model = get_vision_transformer(params)
+    elif params['name'] == 'vit_b16_224_without_pos_emb':
+        model = get_vit_b16_224_without_pos_emb(params)
+    elif params['name'] == 'custom_vision_transformer_without_pos_emb':
+        model = get_vision_transformer_without_pos_emb(params)
+
     else:
         raise NotImplementedError(f'Model {params["name"]} is not implemented')
 
@@ -104,6 +112,68 @@ def get_vit_b16_224(params: dict):
     if out_features is not None:
         model.heads[0] = torch.nn.Linear(in_features=model.heads[0].in_features, out_features=out_features)
         model.num_classes = out_features
+
+    if checkpoint_path is not None:
+        model.load_state_dict(torch.load(checkpoint_path))
+
+    return model
+
+def get_vit_b16_224_without_pos_emb(params: dict):
+    """
+    Returns a pre-trained vision transformer, with the following params:
+    image_size=224
+    input_channels=3,
+    patch_size=16,
+    num_layers=12,
+    num_heads=12,
+    hidden_dim=768,
+    mlp_dim=3072,
+
+    :param params:
+    :return:
+    """
+    out_features = params.get('out_features', None)
+    pretrained = params.get('pretrained', False)
+    checkpoint_path = params.get('checkpoint_path', None)
+
+    model = vit_b_16_without_pos_emb(pretrained=pretrained)
+
+    if out_features is not None:
+        model.heads[0] = torch.nn.Linear(in_features=model.heads[0].in_features, out_features=out_features)
+        model.num_classes = out_features
+
+    if checkpoint_path is not None:
+        model.load_state_dict(torch.load(checkpoint_path))
+
+    return model
+
+def get_vision_transformer_without_pos_emb(params):
+    """
+    Returns a pre-trained vision transformer, with patch size 16x16
+    :param params:
+    :return:
+    """
+    input_channels = params.get('input_channels', None)
+    image_size = params.get('image_size', None)
+    patch_size = params.get('patch_size', None)
+    num_layers = params.get('num_layers', None)
+    num_heads = params.get('num_heads', None)
+    hidden_dim = params.get('hidden_dim', None)
+    mlp_dim = params.get('mlp_dim', None)
+    num_classes = params.get('out_features', None)
+    checkpoint_path = params.get('checkpoint_path', None)
+
+    model = VisionTransformerWithoutPosEmb(
+        image_size=image_size,
+        patch_size=patch_size,
+        num_layers=num_layers,
+        num_heads=num_heads,
+        hidden_dim=hidden_dim,
+        mlp_dim=mlp_dim,
+        num_classes=num_classes)
+
+    if input_channels is not None and input_channels != 3:
+        model.conv1 = torch.nn.Conv2d(input_channels, model.conv1.out_channels, kernel_size=model.conv1.kernel_size, stride=model.conv1.stride, padding=model.conv1.padding, bias=False)
 
     if checkpoint_path is not None:
         model.load_state_dict(torch.load(checkpoint_path))
