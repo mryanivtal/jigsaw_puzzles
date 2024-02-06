bfdd4878d714d505123a543522d682d51b5d0b07
mr.yaniv.tal@gmail.com <mr.yaniv.tal@gmail.com>
2024-02-06 09:05:14 +0200

    params and adamW optimizer

--- src/trainer/factories/optimizer_factory.py
+++ src/trainer/factories/optimizer_factory.py
@@ -1,12 +1,12 @@
 import torch.nn
 from torch.nn import BCELoss, CrossEntropyLoss
-from torch.optim import Adam
+from torch.optim import Adam, AdamW
 
 
 def get_optimizer(params: dict, model):
     if params['name'] == 'adam':
         lr = params.get('lr', 1e-3)
-        optimizer = Adam(model.parameters(), lr=lr)
+        optimizer = AdamW(model.parameters(), lr=lr)
     else:
         raise NotImplementedError(f'optimizer {params["name"]} is not implemented')
 
