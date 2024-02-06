4f66de9040fbee26c1c703430d79b9d8355876bb
mr.yaniv.tal@gmail.com <mr.yaniv.tal@gmail.com>
2024-02-06 10:14:54 +0200

    Added SGD with momentum

--- src/trainer/factories/optimizer_factory.py
+++ src/trainer/factories/optimizer_factory.py
@@ -1,12 +1,18 @@
 import torch.nn
 from torch.nn import BCELoss, CrossEntropyLoss
-from torch.optim import Adam, AdamW
+from torch.optim import Adam, AdamW, SGD
 
 
 def get_optimizer(params: dict, model):
     if params['name'] == 'adamW':
         lr = params.get('lr', 1e-3)
         optimizer = AdamW(model.parameters(), lr=lr)
+
+    elif params['name'] == 'SGD':
+        lr = params.get('lr', 1e-3)
+        momentum = params.get('momentum', 0)
+        optimizer = SGD(model.parameters(), lr=lr, momentum=momentum)
+
     else:
         raise NotImplementedError(f'optimizer {params["name"]} is not implemented')
 
