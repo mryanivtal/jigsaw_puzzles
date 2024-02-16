69b86269b56e28de69d9105ffc6bd0b8e56bf0d5
mr.yaniv.tal@gmail.com <mr.yaniv.tal@gmail.com>
2024-02-16 15:52:25 +0200

    configs

--- src/env_constants.py
+++ src/env_constants.py
@@ -2,9 +2,14 @@ from pathlib import Path
 
 ENVIRONMENT = 'Local'
 
+# ------------- Local computer
 PROJECT_PATH = Path(__file__).parent.parent.parent
 TRAIN_DATA_PATH = PROJECT_PATH / Path('data/train')
-# TRAIN_DATA_PATH = PROJECT_PATH / Path('data/animals_and_more')
-
 TEST_DATA_PATH = PROJECT_PATH / Path('data/test')
+
+# -------------- RUNI server
+# PROJECT_PATH = '/home/mlds_user/yaniv'
+# TRAIN_DATA_PATH = PROJECT_PATH / Path('/home/mlds_user/yaniv/data/train')
+# TEST_DATA_PATH = PROJECT_PATH / Path('/home/mlds_user/yaniv/data/test')
+
 
